82413bd6b1496b34e46a493740e53b16a49dc241
Lee-na-yun <happyin3333@nate.com>
2023-02-10 20:11:07 +0900

    state를 이용하여 주사위를 3번 이미지로 바꿔보기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import { useState } from "react";
 import Button from "./Button";
 import Dice from "./Dice";
 
 function App() {
+  const [num, setNum] = useState(1);
+  const handleRollClick = ()=>{
+    setNum(3);
+  }; //num state를 3으로 바꾸는 함수
+
   return (
     <div>
       <div>
-        <Button>던지기</Button>
+        <Button onClick={handleRollClick}>던지기</Button>
         <Button>처음부터</Button>
       </div>
-      <Dice color="red" num={2} />
+      <Dice color="red" num={num} />
     </div>
   );
 }
