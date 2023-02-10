f6b689b50ff966cd003232783efc5dbbcbce1f64
Lee-na-yun <happyin3333@nate.com>
2023-02-10 20:43:16 +0900

    state로 주사위 던지기의 기록 나열하기

--- src/App.js
+++ src/App.js
@@ -9,16 +9,20 @@ function random(n) {
 function App() {
   const [num, setNum] = useState(1);
   const [sum, setSum] = useState(0); //총점 구하는 state -> 초기값 0으로 줌
+  const [gameHistory, setGameHistory] = useState([]); // 빈배열을 초기값으로 받는 state
 
   const handleRollClick = () => {
     const nextNum = random(6);
     setNum(nextNum);
     setSum(sum + nextNum); // 총점
+    gameHistory.push(nextNum); //gameHistory가 배열이니까 push 메소드로 nextNum을 추가하고
+    setGameHistory(gameHistory); //새 값이 추가된 gameHistory State를 전달
   }; //num state를 바꾸는 함수
 
   const handleClearClick = () => {
     setNum(1);
     setSum(0);
+    setGameHistory([]);
   }; //초기화 기능
 
   return (
@@ -29,9 +33,11 @@ function App() {
       </div>
       <div>
         <h2>나</h2>
-        <Dice color="blue" num={num}/>
-        <h2>총점</h2> 
-        <p>{sum}</p> 
+        <Dice color="blue" num={num} />
+        <h2>총점</h2>
+        <p>{sum}</p>
+        <h2>기록</h2>
+        <p>{gameHistory.join(", ")}</p>
       </div>
     </div>
   );
