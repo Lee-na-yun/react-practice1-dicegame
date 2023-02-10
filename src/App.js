f9a0580600c33ce0719668555691e6e4c0339123
Lee-na-yun <happyin3333@nate.com>
2023-02-10 20:14:30 +0900

    state로 랜덤 버튼 만들기

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import { useState } from "react";
 import Button from "./Button";
 import Dice from "./Dice";
 
+function random(n) {
+  return Math.ceil(Math.random() * n);
+} // 파라미터 n으로 숫자값을 전달 받아서 1부터 n까지의 랜덤한 정수를 반환하는 함수
+
 function App() {
   const [num, setNum] = useState(1);
-  const handleRollClick = ()=>{
-    setNum(3);
+
+  const handleRollClick = () => {
+    const nextNum = random(6);
+    setNum(nextNum);
   }; //num state를 3으로 바꾸는 함수
 
   return (
