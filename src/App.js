f771b0ea7f4bbb2821225879558827dfa666ec61
Lee-na-yun <happyin3333@nate.com>
2023-02-17 11:39:22 +0900

    공통된 데이터를 부모App에게 넘기기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
 import Board from "./Board";
+import { useState } from "react";
+import Button from "./Button";
+
+function random(n) {
+  return Math.ceil(Math.random() * n);
+} // 파라미터 n으로 숫자값을 전달 받아서 1부터 n까지의 랜덤한 정수를 반환하는 함수
 
 function App() {
-  return(
+  const [num, setNum] = useState(1);
+  const [sum, setSum] = useState(0); //총점 구하는 state -> 초기값 0으로 줌
+  const [gameHistory, setGameHistory] = useState([]); // 빈배열을 초기값으로 받는 state
+  const [othernum, setOtherNum] = useState(1);
+  const [othersum, setOtherSum] = useState(0); //총점 구하는 state -> 초기값 0으로 줌
+  const [othergameHistory, setOtherGameHistory] = useState([]); // 빈배열을 초기값으로 받는 state
+
+  const handleRollClick = () => {
+    const nextNum = random(6);
+    const nextOtherNum = random(6);
+    setNum(nextNum);
+    setSum(sum + nextNum); // 총점
+    // gameHistory.push(nextNum); //gameHistory가 배열이니까 push 메소드로 nextNum을 추가하고
+    setGameHistory([...gameHistory, nextNum]); //새 값이 추가된 gameHistory State를 전달
+    setOtherNum(nextOtherNum);
+    setOtherSum(sum+nextOtherNum);
+    setOtherGameHistory([...othergameHistory, nextOtherNum]);
+  }; //num state를 바꾸는 함수
+
+  const handleClearClick = () => {
+    setNum(1);
+    setSum(0);
+    setGameHistory([]);
+    setOtherNum(1);
+    setOtherSum(0);
+    setOtherGameHistory([]);
+  }; //초기화 기능
+
+  return (
     <div>
-      <Board name="나" color="blue"/>
-      <Board name="상대" color="red"/>
+      <div>
+        <div>
+          <Button onClick={handleRollClick}>던지기</Button>
+          <Button onClick={handleClearClick}>처음부터</Button>
+        </div>
+        <Board name="나" color="blue" num={num} sum={sum} gameHistory={gameHistory} /> 
+        <Board name="상대" color="red" num={othernum} sum={othersum} gameHistory={othergameHistory} />
+      </div>
     </div>
   );
 }
--- src/Board.js
+++ src/Board.js
@@ -1,36 +1,10 @@
-import { useState } from "react";
-import Button from "./Button";
 import Dice from "./Dice";
 
-function random(n) {
-  return Math.ceil(Math.random() * n);
-} // 파라미터 n으로 숫자값을 전달 받아서 1부터 n까지의 랜덤한 정수를 반환하는 함수
 
 function Board({name, color}) {
-  const [num, setNum] = useState(1);
-  const [sum, setSum] = useState(0); //총점 구하는 state -> 초기값 0으로 줌
-  const [gameHistory, setGameHistory] = useState([]); // 빈배열을 초기값으로 받는 state
-
-  const handleRollClick = () => {
-    const nextNum = random(6);
-    setNum(nextNum);
-    setSum(sum + nextNum); // 총점
-    // gameHistory.push(nextNum); //gameHistory가 배열이니까 push 메소드로 nextNum을 추가하고
-    setGameHistory([...gameHistory, nextNum]); //새 값이 추가된 gameHistory State를 전달
-  }; //num state를 바꾸는 함수
-
-  const handleClearClick = () => {
-    setNum(1);
-    setSum(0);
-    setGameHistory([]);
-  }; //초기화 기능
 
   return (
-    <div>
-      <div>
-        <Button onClick={handleRollClick}>던지기</Button>
-        <Button onClick={handleClearClick}>처음부터</Button>
-      </div>
+    
       <div>
         <h2>{name}</h2>
         <Dice color={color} num={num} />
@@ -39,7 +13,6 @@ function Board({name, color}) {
         <h2>기록</h2>
         <p>{gameHistory.join(", ")}</p>
       </div>
-    </div>
   );
 }
 
