1b53582b0727f969e4dd93ba9d6a2096329bd859
Lee-na-yun <happyin3333@nate.com>
2023-02-17 12:07:57 +0900

    코드 간단화하기

--- src/App.js
+++ src/App.js
@@ -7,31 +7,19 @@ function random(n) {
 } // 파라미터 n으로 숫자값을 전달 받아서 1부터 n까지의 랜덤한 정수를 반환하는 함수
 
 function App() {
-  const [num, setNum] = useState(1);
-  const [sum, setSum] = useState(0); //총점 구하는 state -> 초기값 0으로 줌
-  const [gameHistory, setGameHistory] = useState([]); // 빈배열을 초기값으로 받는 state
-  const [othernum, setOtherNum] = useState(1);
-  const [othersum, setOtherSum] = useState(0); //총점 구하는 state -> 초기값 0으로 줌
+  const [mygameHistory, setMyGameHistory] = useState([]); // 빈배열을 초기값으로 받는 state
   const [othergameHistory, setOtherGameHistory] = useState([]); // 빈배열을 초기값으로 받는 state
 
   const handleRollClick = () => {
-    const nextNum = random(6);
+    const nextMyNum = random(6);
     const nextOtherNum = random(6);
-    setNum(nextNum);
-    setSum(sum + nextNum); // 총점
     // gameHistory.push(nextNum); //gameHistory가 배열이니까 push 메소드로 nextNum을 추가하고
-    setGameHistory([...gameHistory, nextNum]); //새 값이 추가된 gameHistory State를 전달
-    setOtherNum(nextOtherNum);
-    setOtherSum(sum+nextOtherNum);
+    setMyGameHistory([...mygameHistory, nextMyNum]); //새 값이 추가된 gameHistory State를 전달
     setOtherGameHistory([...othergameHistory, nextOtherNum]);
   }; //num state를 바꾸는 함수
 
   const handleClearClick = () => {
-    setNum(1);
-    setSum(0);
-    setGameHistory([]);
-    setOtherNum(1);
-    setOtherSum(0);
+    setMyGameHistory([]);
     setOtherGameHistory([]);
   }; //초기화 기능
 
@@ -42,8 +30,8 @@ function App() {
           <Button onClick={handleRollClick}>던지기</Button>
           <Button onClick={handleClearClick}>처음부터</Button>
         </div>
-        <Board name="나" color="blue" num={num} sum={sum} gameHistory={gameHistory} /> 
-        <Board name="상대" color="red" num={othernum} sum={othersum} gameHistory={othergameHistory} />
+        <Board name="나" color="blue" gameHistory={mygameHistory} /> 
+        <Board name="상대" color="red" gameHistory={othergameHistory} />
       </div>
     </div>
   );
