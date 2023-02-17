fd86aa8832c77420957ad8e94e765a6a280f069f
Lee-na-yun <happyin3333@nate.com>
2023-02-17 11:22:32 +0900

    기존 코드를 Board로 컴포넌트화하기

--- src/App.js
+++ src/App.js
@@ -1,44 +1,10 @@
-import { useState } from "react";
-import Button from "./Button";
-import Dice from "./Dice";
-
-function random(n) {
-  return Math.ceil(Math.random() * n);
-} // 파라미터 n으로 숫자값을 전달 받아서 1부터 n까지의 랜덤한 정수를 반환하는 함수
+import Board from "./Board";
 
 function App() {
-  const [num, setNum] = useState(1);
-  const [sum, setSum] = useState(0); //총점 구하는 state -> 초기값 0으로 줌
-  const [gameHistory, setGameHistory] = useState([]); // 빈배열을 초기값으로 받는 state
-
-  const handleRollClick = () => {
-    const nextNum = random(6);
-    setNum(nextNum);
-    setSum(sum + nextNum); // 총점
-    // gameHistory.push(nextNum); //gameHistory가 배열이니까 push 메소드로 nextNum을 추가하고
-    setGameHistory([...gameHistory, nextNum]); //새 값이 추가된 gameHistory State를 전달
-  }; //num state를 바꾸는 함수
-
-  const handleClearClick = () => {
-    setNum(1);
-    setSum(0);
-    setGameHistory([]);
-  }; //초기화 기능
-
-  return (
+  return(
     <div>
-      <div>
-        <Button onClick={handleRollClick}>던지기</Button>
-        <Button onClick={handleClearClick}>처음부터</Button>
-      </div>
-      <div>
-        <h2>나</h2>
-        <Dice color="blue" num={num} />
-        <h2>총점</h2>
-        <p>{sum}</p>
-        <h2>기록</h2>
-        <p>{gameHistory.join(", ")}</p>
-      </div>
+      <Board name="나" color="blue"/>
+      <Board name="상대" color="red"/>
     </div>
   );
 }
