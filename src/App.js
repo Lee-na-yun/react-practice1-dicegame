c4e9580aeb560bc10087c8f5f02ae45dac9b71f3
Lee-na-yun <happyin3333@nate.com>
2023-02-17 18:00:14 +0900

    className으로 스타일 적용하기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Board from "./Board";
 import { useState } from "react";
 import Button from "./Button";
+import './App.css';
 
 function random(n) {
   return Math.ceil(Math.random() * n);
@@ -24,11 +25,11 @@ function App() {
   }; //초기화 기능
 
   return (
-    <div>
+    <div className="App">
       <div>
         <div>
-          <Button color="blue" onClick={handleRollClick}>던지기</Button>
-          <Button color="red" onClick={handleClearClick}>처음부터</Button>
+          <Button className="App-button" color="blue" onClick={handleRollClick}>던지기</Button>
+          <Button className="App-button" color="red" onClick={handleClearClick}>처음부터</Button>
         </div>
         <Board name="나" color="blue" gameHistory={mygameHistory} /> 
         <Board name="상대" color="red" gameHistory={othergameHistory} />
--- src/Button.js
+++ src/Button.js
@@ -1,32 +1,10 @@
-const baseButtonStyle = {
-  padding: "14px 27px",
-  border: "solid 1px #7090ff",
-  outline: "none",
-  color: "#7090ff",
-  cursor: "pointer",
-  backgroundColor: "rgba(0,89,255,0.2)",
-  borderRadius: "30px",
-  fontSize: "17px",
-};
+import './Button.css';
 
-const blueButtonStyle = {
-    ...baseButtonStyle,
-    backgroundColor: 'rgba(0,89,255,0.2)',
-    border:'solid 1px #7090ff',
-    color:'#7090ff',
-};
-
-const redButtonStyle = {
-    ...baseButtonStyle,
-    backgroundColor: 'rgba(255,78,78,0.2)',
-    border:'solid 1px #ff466f',
-    color:'#ff4664'
-};
-
-function Button({ children, onClick, color }) {
-    const style = color === 'red'? redButtonStyle : blueButtonStyle;
+function Button({ children, onClick, color='blue', className=''}) {
+    const classNames = `Button ${color} ${className}`;
+    //const style = color === 'red'? redButtonStyle : blueButtonStyle;
   return (
-    <button style={style} onClick={onClick}>
+    <button className={classNames} onClick={onClick}>
       {children}
     </button>
   );
